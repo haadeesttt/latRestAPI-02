6819303897e69c3c585a1988f4b5598196c5bcb5
haadeest <destiohrdn.03@gmail.com>
2024-05-14 11:49:40 +0700

    berhasil update dan delete user -- BACKEND FINISH

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -27,7 +27,35 @@ export const getUserById = async (req, res) => {
 export const createUser = async (req, res) => {
     try {
         await user.create(req.body);
-        res.status(201).json({message: 'User created!'});
+        res.status(201).json({ message: 'User created!' });
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        await user.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: 'User updated!' })
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        await user.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ message: 'User deleted!' })
     } catch (error) {
         console.error(error.message)
         res.status(500).json({ message: 'Internal server error' });
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getUsers, getUserById, createUser } from "../controller/userController.js";
+import { getUsers, getUserById, createUser, updateUser, deleteUser } from "../controller/userController.js";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
 router.post('/users', createUser);
+router.patch('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
